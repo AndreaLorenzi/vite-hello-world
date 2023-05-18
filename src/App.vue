5c65f2b12d90fa910ee985332d3b14cc8d3f706d
Your Name <you@example.com>
2023-05-18 10:52:20 +0200

    add

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script>
 import TitleComponent from "./components/titlecomponent.vue";
 import AppHero from "./components/AppHero.vue";
-export default{
-  components :{
+export default {
+  components: {
     TitleComponent,
     AppHero,
-  }
-}
+  },
+};
 </script>
 
 <template>
   <TitleComponent />
-  <AppHero/>
+  <AppHero />
 </template>
 
 <style scoped></style>
